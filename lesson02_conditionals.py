4a49119f08b4baa3eb16693e8faadada5992907e
abdallasaidbigpartner-web <abdallasaidbigpartner@gmail.com>
2026-09-06 07:18:18 +0300

    Update lesson02_conditionals.py

--- lesson02_conditionals.py
+++ lesson02_conditionals.py
@@ -9,7 +9,7 @@ score = 40
 if score >= 90:
    print("Grade: A")
 elif score >= 80 and score < 90:
-     print("Geade: B")
+     print("Grade: B")
 elif score >= 70 and score < 80:
      print("Grade: C")
 else:
